1395b7e0e33ea7be2719ebc092ff0fd62d6b3bd5
marc-ms <myphysics28@gmail.com>
2025-03-20 08:52:11 +0100

    Update script.js

--- javascript/check-income/script.js
+++ javascript/check-income/script.js
@@ -1,14 +1,33 @@
-// search for greater or eauls incomes for user
+// search for greater or equal incomes for user
 // then asc order and console log them
 
-const incomes = [1000, 4567, 345, 750, 670, 2011, 567]
+const incomes = [1000, 4567, 345, 6750, 670, 5011, 567]
 
-//let userIncome = parseInt(prompt("What's your income?", 0))
+const selectedIncomes = []
 
-function incomeCheck(incomesArray) {
+let userIncome = parseInt(prompt("What's your income?", 0))
+
+function incomeCheck(incomesArray, userInc) {
     for (const income of incomesArray) {
-        console.log(income)
+        if (income >= userInc) {
+            selectedIncomes.push(income)
+        }
+    }
+    console.log(selectedIncomes)
+}
+
+function orderArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                const temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+            }
+        }
     }
 }
 
-incomeCheck(incomes)
+incomeCheck(incomes, userIncome)
+orderArray(selectedIncomes)
+console.log(selectedIncomes)

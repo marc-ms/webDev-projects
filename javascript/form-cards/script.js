0dad1cc27f4e210e641560e46b3c8497e8b31b9b
marc-ms <myphysics28@gmail.com>
2025-03-22 15:16:09 +0100

    added localhost

--- javascript/form-cards/script.js
+++ javascript/form-cards/script.js
@@ -1,43 +1,66 @@
 const articlesLayout = document.querySelector('.layout__articles')
 const title = document.querySelector('#title')
 const subtitle = document.querySelector('#subtitle')
-const description = document.querySelector('#description') 
+const description = document.querySelector('#description')
 const form = document.querySelector('.form__create')
 
-let articles = []
+let new_articles = []
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault()
+function createArticle() {
+    form.addEventListener("submit", (e) => {
+        e.preventDefault()
 
-    if (title.value && subtitle.value && description.value) {
-        let article = {
-            title: title.value,
-            subtitle: subtitle.value,
-            description: description.value
+        if (title.value && subtitle.value && description.value) {
+            let article = {
+                title: title.value,
+                subtitle: subtitle.value,
+                description: description.value
+            }
+            new_articles.push(article)
+            localStorage.setItem("articles", JSON.stringify(new_articles))
+            
+
         }
-        articles.push(article)
-        
-    }
-    console.log(articles[0].title)
-    addArticle()
-})
+        console.table(new_articles)
+        form.reset()
+    })
+}
 
-function addArticle() {
-    articlesLayout.innerHTML += `
+
+function layoutArticle(articless) {
+    layout = `
     <article class="layout__card">
                 <header class="card__header">
-                    <p class="header__author">Mike</p>
+                    <p class="header__author">${articless.title}</p>
                 </header>
                 <div class="card__content">
                     <div class="content_title">
-                        <h2>Hello</h2>
+                        <h2>${articless.subtitle}</h2>
                     </div>
 
                     <p class="content__description">
-                        ${articles[0].description}
+                        ${articless.description}
                     </p>
                     <button class="content__btn">Read More</button>
                 </div>
     </article>
     `
+
+    return layout
 }
+
+function addArticle() {
+    articlesLayout.innerHTML = ""
+    new_articles = JSON.parse(localStorage.getItem("articles"))
+
+    if (new_articles.length > 0) {
+        new_articles.forEach(article => {
+            articlesLayout.innerHTML += layoutArticle(article)
+        }
+
+        )
+    };
+}
+
+addArticle()
+createArticle()
